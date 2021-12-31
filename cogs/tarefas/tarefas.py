674628f02347c0795e7d1d67f088171e24eb0c95
Carlos Daniel <66672234+z3oxs@users.noreply.github.com>
2021-12-31 18:25:05 -0300

    Fixed DB connection

--- cogs/tarefas/tarefas.py
+++ cogs/tarefas/tarefas.py
@@ -9,12 +9,6 @@ db = cluster['codify']
 conta = db['conta']
 logs = db['logs']
 
-from pymongo import MongoClient
-cluster = MongoClient(f'mongodb+srv://{user}:{password}{host}')
-db = cluster['codify']
-conta = db['conta']
-logs = db['logs']
-
 class Tarefas(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
