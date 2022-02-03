7fc9355014ba4efe5796911e197ca3f48bbd233e
jvadebossan <jvdebosssan@gmail.com>
2022-02-03 01:03:17 -0300

    ainda tamo arrumando

--- cogs/tarefas/tarefas.py
+++ cogs/tarefas/tarefas.py
@@ -11,10 +11,34 @@ site = db['site']
 logs = db['logs']
 
 async def find_staff():
-    info = site.find_one({'_id': 1})
+    info = site.find_one({'_id': 0})
     staffs = info['staffs']
     boosters = info['boosters']
     return [staffs, boosters]
+
+def get_update(member, db_user):
+
+    discord_member = {
+        name: member.name,
+        pfp: str(member.avatar_url),
+    }
+
+    for user in db_staffs:
+        if user['id'] == member.id:
+            db_user = user
+
+    user = list(user.items())
+    db_user = list(user.items())
+
+    update = {}
+
+    for i in range(0,len(user)):
+        if(user[i][1]!=db_user[i][1]):
+            update[user[i][0]] = user[i][1]
+            
+    
+    
+    
 
 class Tarefas(commands.Cog):
     def __init__(self, bot):
@@ -36,71 +60,69 @@ class Tarefas(commands.Cog):
             #member quant
             guild = self.bot.get_guild(743482187365613641)
             member_count = int(guild.member_count)
-
+            #channels quant
+            channel_count = len(guild.channels)
             #staff quant
             staff_count = 0
-            staffs = []
-            boosters = []
-            new_staffs = []
-            new_boosters = []
 
-            old_info = True
-            already_on_staff = []
-            staff_old_info = {}
-            booster_old_info = {}
+
+            #staffs and boosters
+            staffs, boosters = [], [] # VAI RECEBER DADOS DO SERVER
+            staffs_to_add, boosters_to_add = [], [] # VAI RECEBER STAFFS CASO NÃO ESTEJAM NO BANCO
+            already_on = []
             try:
-                for category in await find_staff():
-                    for member in category:
-                        already_on.append(member['id'])
-                        if member['role'] == 'BOOSTER':
-                            booster_old_info[{'id':member.id, 'info':member}]
-                        else:
-                            staff_old_info[{'id':member.id, 'info':member}]
+                db_staffs, db_boosters = await find_staff() # VAI RECEBER STAFFS DO BANCO
+                def get_all_ids(lista):
+                    for i in lista:
+                        already_on.append(i['id'])
+                get_all_ids(db_staffs)
+                get_all_ids(db_boosters)
             except:
-                old_info = False
-                pass
-
-
+                print('erro')
+                db_staffs, db_boosters = [], []
 
             staff_roles_names = ["⎯⎯⎯⎯⎯⎯⠀〔Admin's〕⎯⎯⎯⎯⎯⎯⎯⠀", "⎯⎯⎯⎯⎯⎯⎯⎯⠀〔Mod〕⎯⎯⎯⎯⎯⎯⎯⎯⎯⠀", '⎯⎯⎯⎯⎯⎯⠀〔Dono〕⎯⎯⎯⎯⎯⎯⎯⠀']
             role_types = {"⎯⎯⎯⎯⎯⎯⠀〔Admin's〕⎯⎯⎯⎯⎯⎯⎯⠀":"ADMIN", "⎯⎯⎯⎯⎯⎯⎯⎯⠀〔Mod〕⎯⎯⎯⎯⎯⎯⎯⎯⎯⠀":"MOD", "⎯⎯⎯⎯⎯⎯⠀〔Dono〕⎯⎯⎯⎯⎯⎯⎯⠀":"OWNER", "BOOSTER ❤️":"boosters"}
             for member in guild.members:
-                for e in member.roles:
-                    if e.name in staff_roles_names and member.id not in already_on_staff:
-                        staff_count += 1
-                        role = role_types[member.top_role.name]
-                        new_staffs.append({'id': member.id, 'name': member.name, 'pfp': str(member.avatar_url), 'bio':'Biografia Não Definida', 'ocupacao':'', 'habilidades':['Nenhuma'], 'github':'https://github.com/codify-community', 'role':role})
-                        staffs.append(member)
-                    if e.name == "BOOSTER ❤️" and member.id not in already_on_staff:
-                        staff_count += 1
-                        new_boosters.append({'id': member.id, 'name': member.name, 'pfp': str(member.avatar_url), 'bio':'Biografia Não Definida', 'ocupacao':'', 'habilidades':['Nenhuma'], 'github':'https://github.com/codify-community', 'role':'BOOSTER'})
-                        boosters.append(member)
-            #channels quant
-            channel_count = len(guild.channels)
+                for role in member.roles:
+                    if role.name in staff_roles_names:
+                        if member.id not in already_on:
+                            staff_count += 1
+                            role = role_types[member.top_role.name]
+                            new_staffs.append({'id': member.id, 'name': member.name, 'pfp': str(member.avatar_url), 'bio':'Biografia Não Definida', 'ocupacao':'', 'habilidades':['Nenhuma'], 'github':'https://github.com/codify-community', 'role':role})
+                        else:
 
-            #verify if name or pfp changed
-            if old_info:
-                role_list = [[staffs, boosters], [[staff_old_info, new_staffs], [booster_old_info, new_boosters]]] #0 = staff, 1 = booster
-                for e in range(2):
-                    for member in role_list[0][e]:
-                        for i in ['name', 'pfp']:
-                            if role_list[1][e][0][member.id]['info'][i] != member.name:
-                                print(f'Nome ou pfp de {member.name} mudou')
-                                role_list[1][e][0][member.id]['info'][i] = member.name
-                                role_list[1][e][1].append(role_list[1][e][0][member.id]['info'])
+                            
+
+
+
+                            db_discord_member = {
+
+                            }
+
+
+                            get_update({
+                                id: member.id,
+
+                            }, {
+                                id: ""
+                            })
+                            
+                            
+
+                    if role.name == "BOOSTER ❤️"  and member.id not in already_on:
+                        new_boosters.append({'id': member.id, 'name': member.name, 'pfp': str(member.avatar_url), 'bio':'Biografia Não Definida', 'ocupacao':'', 'habilidades':['Nenhuma'], 'github':'https://github.com/codify-community', 'role':'BOOSTER'})
             
-            #push info to site collection
-            print('PUSHING INFO TO MONGO')
-            site.find_one_and_update({'_id': 1}, {'$set': {'member_count': member_count, 'staff_count': staff_count, 'channel_count': channel_count}})
-            if len(new_staffs) + len(new_boosters) > 0:
-                print('NEW STAFF FOUND')
-                def update_members_db(members, type):
-                    for new in members:
-                        site.find_one_and_update({'_id': 1}, {'$push': {type: new}})
-                update_members_db(new_staffs, 'staffs')
-                update_members_db(new_boosters, 'boosters')
-                print('NEW STAFF PUSHED')
-            print('INFO PUSHED')
+
+            
+
+
+            print(f'''
+            staffs: {staffs}
+            boosters: {boosters}
+            ''')
+            #site.update_one({'_id':0}, {'$set':{'channel_count':channel_count, 'staff_count':staff_count, 'member_count':member_count, 'staffs':staffs, 'boosters':boosters}})
+
         get_info.start(self)
 
 def setup(bot):
